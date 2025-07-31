940bb4c1b965214b0e85ef5b52076a1738171941
Abdul Majeed <dev.abdul.majeed@gmail.com>
2025-07-31 15:01:05 +0100

    Added home button to nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def after_sign_in_path_for(resource)
+    case resource
+    when SuperAdmin
+      super_admin_home_path
+    when Admin
+      admin_home_path
+    when Teacher
+      teacher_home_path
+    when Student
+      student_home_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to root_path, alert: "You need to sign in first."
+    end
+  end
 end
